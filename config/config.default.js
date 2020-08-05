ee4774e256bdf43db048cc68d4efcd24b5278beb
suchengyong <suchengyong520@163.com>
2020-08-05 19:33:02 +0800

    更新提交session配置

--- config/config.default.js
+++ config/config.default.js
@@ -12,10 +12,17 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
+  // 配置cookie 加密的密钥
   config.keys = appInfo.name + '_1596281234178_4548';
-
-  // add your middleware config here
+  // 配置session 的过期时间和一些基本配置
+  config.session = {
+    key:'SESSION_ID',//设置session cookie里面的key
+    maxAge:5000,//5秒钟过期时间默认过期时间一天
+    httpOnly:true,//不让js修改
+    encrypt:true,//加密
+    renew:true,// 每次刷新页面都会重新 计算延期时间
+  }
+  // add your middleware config here 添加中间价
   config.middleware = [
     //'auth' // 白名单 需要登录才能访问的URL方式 和config.auth里面路由，和文件middleware/auth.js 配合做白名单路由拦截
   ];//中间价放到在里
@@ -37,6 +44,7 @@ module.exports = appInfo => {
       '.html':'ejs'
     }
   }
+
   // add your user config here
   const userConfig = {
     mysql:{
